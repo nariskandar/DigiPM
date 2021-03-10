70bf38ac7927adeea16abaab20909a6dcc2ab036
nariskandar <silvercodee@gmail.com>
2021-03-10 13:23:07 +0700

    Homepage Many Task

---
+++ build/app/generated/source/buildConfig/release/mobile/icsp/digi_pm_skin/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package mobile.icsp.digi_pm_skin;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "mobile.icsp.digi_pm_skin.production";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 15;
+  public static final String VERSION_NAME = "1.5.7";
+}
